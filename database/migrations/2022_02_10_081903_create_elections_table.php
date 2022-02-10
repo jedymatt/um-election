d6cfe655a0aa2f92ed659c56289946ca2fbd05d3
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-02-10 08:36:33 +0000

    Added Position,  Student, DepartmentOperator, and Election's models and migrations

---
+++ database/migrations/2022_02_10_081903_create_elections_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('elections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('election_type_id')->constrained();
+            $table->string('title')->unique();
+            $table->string('description');
+            $table->dateTime('start_at');
+            $table->dateTime('end_at');
+            $table->string('school_year');
+            $table->boolean('is_open');
+            $table->foreignId('department_id');
+
+
+
+
+
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('elections');
+    }
+};
