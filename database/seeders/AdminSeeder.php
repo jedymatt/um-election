3603261d557c34fac235e5e9ac3a78db7baab5b5
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-02-26 21:52:31 +0800

    Added AdminSeeder.php
    Seeding superadmin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin.tc@umindanao.edu.ph',
+            'password' => bcrypt('password'),
+            'is_superadmin' => true,
+        ]);
+    }
+}
