a34a3fce55d8cace4595f5d89c3e3dbd681680ca
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-02-26 18:39:30 +0800

    Added DepartmentSeeder.php

---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Department;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $departments = [
+            ['name' => 'Department of Arts and Science'],
+            ['name' => 'Department of Accounting Education'],
+            ['name' => 'Department of Business Administration Education'],
+            ['name' => 'Department of Teachers Education'],
+            ['name' => 'Department of Engineering Education'],
+            ['name' => 'Department of Criminal Justice Education'],
+            ['name' => 'Hospitality Management and Tourism Management'],
+        ];
+
+        Department::insert($departments);
+    }
+}
